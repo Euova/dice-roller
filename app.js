7ecde5edb3587352ab64254efbe2d9f21863ed59
Eyad Nasr <eyadnasr8@gmail.com>
2025-02-27 14:43:36 +0400

    fixed app.js

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const app = express();
 const port = 8080; // Azure expects 8080
 
+app.use(cors());
+
 app.get('/roll-dice', (req, res) => {
   const diceValue = Math.floor(Math.random() * 5 + 1);
   res.json({ value: diceValue });
-  console.log(res);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Dice app listening on port ${port}`)
 })
