c1151a62a09fb8c0277c18b014308de3b9d52bff
Eyad Nasr <eyadnasr8@gmail.com>
2025-02-27 14:28:10 +0400

    saved express

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 8080; // Azure expects 8080
 
 app.get('/roll-dice', (req, res) => {
   const diceValue = Math.floor(Math.random() * 5 + 1);
   res.json({ value: diceValue });
+  console.log(res);
 })
 
 app.listen(port, () => {
--- script.js
+++ script.js
@@ -3,12 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     rollBtn.addEventListener('click', () => {
         let diceValuePlaceholder = document.querySelector('#dice-result');
+        let diceValue;
         fetch()
             .then(response => response.json())
             .then(dice => {
                 diceValue = dice.value;
             })
             .catch(err => console.error('error: ' + err));
+            
+        
         diceValuePlaceholder.textContent = diceValue;
     });
 
